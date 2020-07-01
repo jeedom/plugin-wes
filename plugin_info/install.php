cc807d2d11580a295d7cdab84b1163ecb6cad417
Rocket13011 <alexandre@jeedom.com>
2020-07-01 14:08:23 +0200

    fix ancien plugin vers le nouveau

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -35,6 +35,66 @@ function wes_install() {
 }
 
 function wes_update() {
+	$analogiques = eqLogic::byType('wes_analogique');
+	$boutons = eqLogic::byType('wes_bouton');
+	$compteurs = eqLogic::byType('wes_compteur');
+	$pinces = eqLogic::byType('wes_pince');
+	$relais = eqLogic::byType('wes_relai');
+	$teleinfos = eqLogic::byType('wes_teleinfo');
+	$temperatures = eqLogic::byType('wes_temperature');
+	
+	foreach ($analogiques as $analogique){
+		if(isObject($analogique)){
+			$analogique->setEqType_name('wes');
+			$analogique->save();
+		}
+	}
+	
+	foreach ($boutons as $bouton){
+		if(isObject($bouton)){
+			$bouton->setEqType_name('wes');
+			$bouton->save();
+		}
+	}
+	
+	foreach ($compteurs as $compteur){
+		if(isObject($compteur)){
+			$compteur->setEqType_name('wes');
+			$compteur->save();
+		}
+	}
+	
+	foreach ($relais as $relai){
+		if(isObject($relai)){
+			$relai->setEqType_name('wes');
+			$relai->save();
+		}
+	}
+	
+	foreach ($pinces as $pince){
+		if(isObject($pince)){
+			$pince->setEqType_name('wes');
+			$pince->save();
+		}
+	}
+	
+	foreach ($teleinfos as $teleinfo){
+		if(isObject($teleinfo)){
+			$teleinfo->setEqType_name('wes');
+			$teleinfo->save();
+		}
+	}
+	
+	foreach ($temperatures as $temperature){
+		if(isObject($temperature)){
+			$temperature->setEqType_name('wes');
+			$temperature->save();
+		}
+	}
+	
+	
+	
+	
     $cron = cron::byClassAndFunction('wes', 'pull');
 	if (is_object($cron)) {
 		$cron->stop();
